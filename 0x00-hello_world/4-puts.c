e0ee3a33d02eaa97b117d762ea92c6f85b62f9c4
Oussama <oussama@hotmail.com>
2023-06-15 12:28:14 -0700

    change 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/* main - Entry point */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
