0bd74f5d6917aee006a706d5c5feeff95d0459bb
Oussama <oussama@hotmail.com>
2023-06-16 19:06:23 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 
 for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-putchar('alphabet');
+putchar(alphabet);
 
 putchar('\n');
 
