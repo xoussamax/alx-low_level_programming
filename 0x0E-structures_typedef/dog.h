88737ae5072b0c8922ac1b8c28bbcc7fa8d970c2
Oussama <oussama@hotmail.com>
2023-07-17 16:01:00 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog
+struct dog /*- Represents information about a dog.*/
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 #endif
