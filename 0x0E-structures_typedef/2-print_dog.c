7f77d045270e08d6ad9f904742aa1edffd32a13b
Oussama <oussama@hotmail.com>
2023-07-17 17:04:06 -0700

    tasks

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,19 +18,3 @@ void print_dog(struct dog *d)
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	struct dog my_dog;
-
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	print_dog(&my_dog);
-	return (0);
-}
