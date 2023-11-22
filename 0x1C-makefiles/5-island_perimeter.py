e9b967a6929063bcdd2232a9cd372957db659be7
oussama amahrouch <oussama_98@hotmail.fr>
2023-11-23 00:14:53 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+""Defines an island perimeter measuring function.""
 
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
+    """
+    Return the perimiter of an island.
 
     The grid represents water by 0 and land by 1.
 
